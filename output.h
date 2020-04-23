18fd1648ada07cd879079850736449938a948269
adEseH <51024254+adEseH@users.noreply.github.com>
2020-04-23 17:57:36 +0200

    neu

--- output.h
+++ output.h
@@ -10,10 +10,10 @@ void output(cell *field,int imax,int jmax)
     pressure = fopen ("pressure.txt","w+");
     for(int j=0;j<jmax;j++){
         for(int i=0;i<imax;i++){
-            fprintf(vspeed,"%.6f\t",field[i+jmax*j].u);
-            fprintf(uspeed,"%.6f\t",field[i+jmax*j].v);
-            fprintf(pressure,"%.6f\t",field[i+jmax*j].p);
-            //printf("%d",i+jmax*j);
+            fprintf(vspeed,"%.6f\t",field[i+imax*j].u);
+            fprintf(uspeed,"%.6f\t",field[i+imax*j].v);
+            fprintf(pressure,"%.6f\t",field[i+imax*j].p);
+            //printf("%d",i+imax*j);
         }
         fprintf(vspeed,"\n");
         fprintf(uspeed,"\n");
