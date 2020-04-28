c44b4a6c86351490c9b908ecf904b794397b4b3c
adEseH <51024254+adEseH@users.noreply.github.com>
2020-04-28 13:09:53 +0200

    neu

--- xandyvalues.h
+++ xandyvalues.h
@@ -9,25 +9,31 @@ xandy *put_xy_in_file(int imax, int jmax, float dx, float dy){
     xandy *c;
     c = calloc(imax + jmax,sizeof(xandy));
 
-    FILE * xy;
-    xy = fopen("x_and_y.txt", "w+");
+    FILE * xv;
+    xv = fopen("x_values.txt", "w+");
+
+	
+	FILE * yv;
+    yv = fopen("y_values.txt", "w+");
 
     for(int i = 0; i < imax; i++){
         c[i].x  = xwert;
-        fprintf(xy, "%.6f\t", xwert);
+        fprintf(xv, "%.6f\t", xwert);
         xwert += dx;  
     }
 
-    fprintf(xy, "\n");
+    //fprintf(xy, "\n");
 
     for(int j = 0; j < jmax; j++){
         c[j].y = ywert;
-        fprintf(xy, "%.6f\t", ywert);
+        fprintf(yv, "%.6f\t", ywert);
         ywert += dy;
         //printf("%.2f\n",y[j]);
     }
 
-    int fclose (FILE *xy);
+    int fclose (FILE *xv);
+    int fclose (FILE *yv);
+
     return c; 
 
 }
