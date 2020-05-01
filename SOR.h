12d4310ee667d79c52fd3a90faedfc0fdb5df596
adEseH <51024254+adEseH@users.noreply.github.com>
2020-05-01 14:32:31 +0200

    es funktioniert

--- SOR.h
+++ SOR.h
@@ -7,36 +7,105 @@
 // p_it+1 function 
 //returns a float
 float p_it(float xpi, float x, float xmi, float xpj, float xmimj, float RHS, float dx, float dy, float omega){
-return (1 - omega)*x + omega * (1/ (2*( 1/pow(dx,2) + 1/pow(dy,2)) ) ) * 
+
+printf("xpi: %.6f, x:  %.6f, xmi: %.6f,xpj: %.6f,xmimj %.6f, RHS %.6f, dx; %.6f, dy: %.6f, omega: %.6f\n", xpi,  x, xmi,  xpj, xmimj, RHS,  dx, dy,  omega);
+float p;
+p = (1 - omega)*x + omega * (1/ (2*( 1/pow(dx,2) + 1/pow(dy,2)) ) ) * 
         ( (xpi + xmi)/pow(dx,2)  +  (xpj + xmimj)/pow(dy,2)  -  RHS);
+printf("Druck: %.6f\n",p);
+return p;
 }
 
 // p_it+1 struct
 // returns a struct with all p_it+1 values
 
-//pres p_it_struct()
+
+presit * pres_it(presit * pres, rhs_struct * RHS, float dx, float dy, float omega, float (*p_it)(float,float, float, float,float, float, float,float, float), int imax2, int jmax2){
+
+
+// fill boundary cells
+// imax2: values + boundary values 
+float pbi[imax2], pbj[jmax2];
+
+    // new i boundaries left
+for(int i = 0; i < imax2; i++){
+        pres[i].p = pres[imax2 + i].p;
+        pbi[i] = pres[(jmax2-2)*imax2 + i].p;
+    }
+
+   //printf("\n") ;
+
+// new j boundaries up
+for(int j = 0; j < jmax2; j++ ){
+        pres[imax2*j].p = pres[j*imax2 + 1].p;
+        pbj[j] = pres[imax2 * j + (imax2-2)].p;
+    }
+
+for(int j = 0; j < jmax2; j++){
+    for(int i = 0; i < imax2; i++){
+           printf("%.6f\t", pres[j*imax2 + i].p);
+        }
+    printf("\n");
+    } 
+
+// calculate pressura values p_it + 1 
+    for(int j = 1; j < jmax2 - 1; j++){
+    for(int i = 1; i < imax2 - 1; i++){
+            pres[j*imax2 + i].p = (*p_it)(pres[j*imax2 + i + 1].p, pres[j*imax2 + i ].p, pres[j*imax2 + i -1].p, pres[(j+1)*imax2 + i].p, pres[(j-1)*imax2 + i - 1].p, RHS[imax2 * j + i].rhs, dx, dy, omega);                     
+        }
+    } 
+
+    // new i boundaries right 
+for(int i = 0; i < imax2; i++){
+      pres[(jmax2-1)*imax2 + i].p = pbi[i];
+    }
+
+   //printf("\n") ;
+
+// new j boundaries down
+for(int j = 0; j < jmax2; j++ ){
+       pres[imax2 * j + (imax2-1)].p = pbj[j];
+    }
+
+
+    return pres;}
+
+
+
+
+
+
 
 // RHS
 //returns a struct
+rhs_struct *rhs_func( rhs_struct * RHS, cell_fg * fg, float dx, float dy, float dt, int imax2, int jmax2){
 
-rhs_struct *rhs_func( rhs_struct * RHS, cell_fg * fg, float dx, float dy, float dt, int imax, int jmax){
-
-
-    //Randwerte - müssen nachher noch übergeben werden
-    float f1 = 0;
-    float g1 = 3;
-
-    for(int j = 0; j < JMAX; j++){
-    for(int i = 0; i < IMAX; i++){
-        printf(" f: %.6f\t",fg[i].f);
-        printf(" g: %.6f\t \n",fg[i].g);
+    /*for(int j = 0; j < jmax2; j++){
+    for(int i = 0; i < imax2; i++){
+        printf(" f: %.6f\t",fg[imax2*j + i].f);
+        printf(" g: %.6f\t \n",fg[imax2*j + i].g);
     }
-}
+    }*/
 
+    /*for(int j = 0; j < jmax2; j++){
+    for(int i = 0; i < imax2; i++){
+        printf(" %.6f\t",RHS[imax2*j + i].rhs);
+        
+    }
+    printf("\n");
+    }*/
 
-    for(int j = 0; j < jmax; j++){
-    for(int i = 0; i < imax; i++){
-        if(i == 0 || j == 0){
+    //printf("imax2: %d and jmax2: %d\n", imax2, jmax2);
+    //printf(" %.6f %.6f %.6f\n", dx,dy,dt);
+
+    for(int j = 1; j < jmax2-1; j++){
+    for(int i = 1; i < imax2-1; i++){
+
+        /*if(i == 0 || i == (imax2 -1) || j == 0 || j == jmax2-1 )
+        {
+            continue;
+        }*/
+       /* if(i == 0 || j == 0){
             if( i == 0 && j == 0){
             RHS[imax * j +i].rhs = 1/dt * ( ( fg[imax * j + i].f - f1)/dx + (fg[imax * j + i].g - g1)/dy);
             }
@@ -47,22 +116,28 @@ rhs_struct *rhs_func( rhs_struct * RHS, cell_fg * fg, float dx, float dy, float 
            else{ // wenn was nicht funktionieren hier mal  else if(j == 0 && i != 0) einfügen
                  RHS[imax * j +i].rhs = 1/dt * ( ( fg[imax * j + i].f - fg[imax * j + i-1].f)/dx + (fg[imax * j + i].g - g1)/dy);
                  }
-            }
-        else{
-        RHS[imax * j +i].rhs = 1/dt * ((fg[imax * j + i].f - fg[imax * j + i-1].f) / dx + (fg[imax * j + i].g - fg[imax * (j - 1) + i].g) / dy);
-            }
-        printf("%.6f \t", RHS[imax * j +i].rhs);
+            }*/
+        //else{
+        RHS[imax2 * j +i].rhs = 1/dt * ((fg[imax2 * j + i].f - fg[imax2 * j + i-1].f) / dx + (fg[imax2 * j + i].g - fg[imax2 * (j - 1) + i].g) / dy);
+          //}
         }
-    } //Randwerte noch mit einbauen!!! Das könnte dann schwierig mit if bedingungen werden, da die Randwert immer nach j * imax Werten kommen
-    free(RHS);
+    }
+    
+    // fill the boundaries
+    for(int i = 0; i < imax2; i++){
+            RHS[i].rhs = RHS[imax2 + i].rhs;
+            RHS[(jmax2-1)*imax2 + i].rhs = RHS[(jmax2-2)*imax2 + i].rhs;
+        }
+
+    for(int j = 0; j < jmax2; j++ ){
+        RHS[imax2*j].rhs = RHS[j*imax2 + 1].rhs;
+        RHS[imax2 * j + (imax2-1)].rhs = RHS[imax2 * j + (imax2-2)].rhs;
+    }
     return RHS; 
 }
 
 // residuum
 // returns a struct
-
-
-
 
 
 // abs(residuum) as L_2 norm
@@ -83,3 +158,7 @@ float abs_res(cell * resi, int imax, int jmax){
 }
 // ghost cell
 // fill the fist variables of the struct
+
+
+
+//Moren: als nächstes Das P_it zum laufen bringen
--- SOR_test.c
+++ SOR_test.c
@@ -2,6 +2,9 @@
 #include <math.h>
 #include <stdlib.h>
 
+
+///////// AUFPASSEN WEGEN IMAX UND IMIN IN VORSCHLEIFEN
+
 #define IMAX    2
 #define JMAX    3
 
@@ -9,6 +12,11 @@ typedef struct
 {
     float r;  
 } cell;
+
+typedef struct 
+{
+    float p;  
+} presit;
 
 typedef struct 
 {
@@ -21,10 +29,14 @@ typedef struct
     float g;  
 } cell_fg;
 
+
 #include "allok.h"
 #include "SOR.h"
 
 void main(){
+int imax2, jmax2; 
+imax2 = IMAX + 2;
+jmax2 = JMAX + 2;
 
 // test the abs_residuum_function
 
@@ -48,34 +60,124 @@ residuum = abs_res(a, IMAX, JMAX);
 
 //printf("\n %.6f \n", residuum);
 
+
+
+
+
 // test the RHS function
 
 // fill two fields
 cell_fg * fg;
-fg = calloc(IMAX*JMAX,sizeof(cell_fg));
+fg = calloc((IMAX+2)*(JMAX+ 2),sizeof(cell_fg));
 
-for(int j = 0; j < JMAX; j++){
-    for(int i = 0; i < IMAX; i++){
-        fg[IMAX*j + i].f = 1;
-        fg[IMAX*j + i].g = 1;
-        printf(" f: %.6f\t",fg[i].f);
-        printf(" g: %.6f\t \n",fg[i].g);
+for(int j = 0; j < JMAX + 2; j++){
+    for(int i = 0; i < IMAX + 2; i++){
+        fg[(IMAX+2)*j + i].f = 2*j - i*j;
+        fg[(IMAX+2)*j + i].g = 2*i + i*j;
+        //printf(" f: %.6f\t",fg[(IMAX+2)*j +i].f);
+        //printf(" g: %.6f\t \n",fg[(IMAX+2)*j +i].g);
     }
 }
-
-
 
 // test function
 
 // create a field and inialisize dx, dy, dz
-
 float dx = 1, dy = 1, dt =  1; 
 
 rhs_struct *RHS;
-RHS = calloc(IMAX*JMAX,sizeof(rhs_struct)); 
+RHS = calloc((IMAX+2)*(JMAX+2),sizeof(rhs_struct)); 
 
-rhs_func(RHS, fg ,dx, dy, dt, IMAX, JMAX);
+rhs_func(RHS, fg ,dx, dy, dt, IMAX + 2, JMAX + 2);
 
-free(fg); // frei setzt mir bestimmer werte auf null
+
+printf("\nRHS\n");
+for(int j = 0; j < JMAX + 2; j++){
+    for(int i = 0; i < IMAX + 2; i++){
+        
+        printf(" %.6f\t",RHS[(IMAX+2)*j +i].rhs);
+    }
+    printf("\n");
+}
+
+printf("\n\n");
+
+
+
+
+
+
+
+///// Pressure
+
+// test boundary cells 
+
+presit * pres;
+pres = calloc((IMAX+2)*(JMAX+2),sizeof(presit));
+
+printf("\nDruckwerte0:\n");
+for(int j = 0; j < JMAX+2; j++){
+    for(int i = 0; i < IMAX+2; i++){
+        pres[(IMAX+2)*j + i].p = i*j;
+        printf("%.6f\t",pres[(IMAX+2)*j + i].p);
+    }
+    printf("\n");
+}
+
+ printf("\n");
+/*
+// new i boundaries
+for(int i = 0; i < imax2; i++){
+        pres[i].p = pres[imax2 + i].p;
+        pres[(jmax2-1)*imax2 + i].p = pres[(jmax2-2)*imax2 + i].p;
+    }
+
+  // printf("\n") ;
+
+// new j boundaries
+    for(int j = 0; j < jmax2; j++ ){
+        pres[imax2*j].p = pres[j*imax2 + 1].p;
+        pres[imax2 * j + (imax2-1)].p = pres[imax2 * j + (imax2-2)].p;
+    }
+
+   // printf("\n");
+
+    for(int j = 0; j < JMAX+2; j++){
+    for(int i = 0; i < IMAX+2; i++){
+       // printf("%.6f\t",pres[(IMAX+2)*j + i].p);
+    }
+    printf("\n");
+}
+*/
+
+// test the pres_it function
+
+float omega = 0.8;
+
+pres = pres_it(pres,  RHS, dx,  dy,  omega, p_it, IMAX +2, JMAX + 2);
+
+printf("\n\n");
+printf("Druckwerte1:\n");
+
+for(int j = 0; j < JMAX+2; j++){
+    for(int i = 0; i < IMAX+2; i++){
+       printf("%.6f\t",pres[(IMAX+2)*j + i].p);
+    }
+    printf("\n");
+}
+
+printf("\n");
+
+pres = pres_it(pres,  RHS, dx,  dy,  omega, p_it, IMAX +2, JMAX + 2);
+
+printf("Druckwerte2:\n");
+for(int j = 0; j < JMAX+2; j++){
+    for(int i = 0; i < IMAX+2; i++){
+       printf("%.6f\t",pres[(IMAX+2)*j + i].p);
+    }
+    printf("\n");
+}
+
+printf("\n");
+
 
 }
